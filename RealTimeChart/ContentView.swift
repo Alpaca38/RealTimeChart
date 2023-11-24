3590d9fb3d2f81e9a6a0bde439bbdcd46f6dff63
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-11-24 15:14:40 +0900

    Initial Commit

---
+++ RealTimeChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealTimeChart
+//
+//  Created by 조규연 on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealTimeChart/RealTimeChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeChartApp.swift
+//  RealTimeChart
+//
+//  Created by 조규연 on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
