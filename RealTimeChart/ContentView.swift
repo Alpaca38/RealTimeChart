b46383e7a8ce6fe991b9aa5a844e06f63a678736
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-11-25 22:28:15 +0900

    [Design] 주파수에 따라 막대 색 변경

--- RealTimeChart/ContentView.swift
+++ RealTimeChart/ContentView.swift
@@ -7,9 +7,10 @@
 
 import Charts
 import SwiftUI
+import AVFAudio
 
 enum Constants {
-    static let updateInterval = 0.05
+    static let updateInterval = 0.03
     static let barAmount = 40
     static let magnitudeLimit: Float = 32
 }
@@ -17,6 +18,8 @@ enum Constants {
 struct ContentView: View {
     let audioProcessing = AudioProcessing.shared
     
+    @State var progress: Double = 0.0
+//    @State var currentTime: Double = 0.0
     @State var isPlaying = false
     @State var data: [Float] = Array(repeating: 0, count: Constants.barAmount)
         .map { _ in Float.random(in: 1...Constants.magnitudeLimit) }
@@ -29,24 +32,91 @@ struct ContentView: View {
         ).autoconnect()
         
         VStack {
-            Chart(Array(data.enumerated()), id: \.0) { index, magnitude in
-                BarMark(
-                    x: .value("Frequency", String(index)),
-                    y: .value("Magnitude", magnitude)
+            Spacer()
+            
+            VStack {
+                Chart(Array(data.enumerated()), id: \.0) { index, magnitude in
+                    BarMark(
+                        x: .value("Frequency", String(index)),
+                        y: .value("Magnitude", magnitude)
+                    )
+                    .foregroundStyle(
+                        Color(
+                            hue: 0.3 - Double(magnitude / Constants.magnitudeLimit / 5),
+                            saturation: 1,
+                            brightness: 1,
+                            opacity: 0.7
+                        )
+                    )
+                }
+                .onReceive(timer, perform: updateData)
+                .chartYScale(domain: 0...Constants.magnitudeLimit)
+                .chartXAxis(.hidden)
+                .chartYAxis(.hidden)
+                .frame(height: 100)
+                .padding()
+                .background(
+                    .black
+                        .opacity(0.3)
+                        .shadow(.inner(radius: 20))
                 )
+                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                
+                playerControls
+                    .onReceive(timer, perform: updateProgress)
             }
-            .onReceive(timer, perform: updateData)
-            .chartYScale(domain: 0...Constants.magnitudeLimit)
-            .chartXAxis(.hidden)
-            .chartYAxis(.hidden)
-            .frame(height: 100)
+            .padding()
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .shadow(radius: 40)
+            .padding()
+        }
+        .background {
+            backgroundPicture
+        }
+        .preferredColorScheme(.dark)
+    }
+    
+    var backgroundPicture: some View {
+        AsyncImage(
+            url: URL(
+                string: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Beethoven.jpg"
+            ),
+            transaction: Transaction(animation: .easeOut(duration: 1))
+        ) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFill()
+            default:
+                Color.clear
+            }
+        }
+        .overlay {
+            LinearGradient(
+                colors: [.clear, .black],
+                startPoint: .center,
+                endPoint: .bottom
+            )
+        }
+        .ignoresSafeArea()
+    }
+    
+    var playerControls: some View {
+        Group {
+            ProgressView(value: progress)
+                .tint(.secondary)
+            Text("Beethoven Symphony No. 5 in C minor, Op. 67")
+                .font(.title2)
+                .lineLimit(1)
+            Text("Ludwig van Beethovan")
             
             Button(action: playButtonTapped) {
                 Image(systemName: "\(isPlaying ? "pause" : "play").circle.fill")
                     .resizable()
                     .frame(width: 50, height: 50)
             }
-        .padding()
         }
     }
     
@@ -66,6 +136,31 @@ struct ContentView: View {
                     min($0, Constants.magnitudeLimit)
                 }
             }
+        }
+    }
+    
+    func updateProgress(_: Date) {
+        if isPlaying {
+            var currentTime = getCurrentTime()
+            let duration = getDuration("music")
+            progress = currentTime / Double(duration)
+        }
+    }
+    
+    func getCurrentTime() -> TimeInterval {
+        if let nodeTime: AVAudioTime = audioProcessing.player.lastRenderTime, let playerTime: AVAudioTime = audioProcessing.player.playerTime(forNodeTime: nodeTime) {
+           return Double(Double(playerTime.sampleTime) / playerTime.sampleRate)
+        }
+        return 0
+    }
+    
+    func getDuration(_ fileName: String) -> TimeInterval {
+        do {
+            let audioFile = try AVAudioFile(forReading: Bundle.main.url(forResource: fileName, withExtension: "mp3")!)
+            let audioNodeFileLength = AVAudioFrameCount(audioFile.length)
+            return Double(Double(audioNodeFileLength) / audioFile.fileFormat.sampleRate)
+        } catch {
+            return 0
         }
     }
 }
